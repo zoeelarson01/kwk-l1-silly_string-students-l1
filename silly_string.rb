3c2ddca34140bf015b499239ed804f47271f8049
zoeelarson01 <zoeelarson@gmail.com>
2018-06-04 20:39:23 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,5 +1,5 @@
-puts "Eleemosynary"
-puts "Eleemosynary".upcase
+your_favorite_word = Eleemosynary
+puts "#{your_favorite_word}".upcase
 puts "IM NOT SHOUTING".downcase
 puts "supercalifragilisticexpialidocious".length
 puts "wow" + "mom"
