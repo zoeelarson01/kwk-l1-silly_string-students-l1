30f481a0fa87cfea17c165b71076d77fe240b6f3
zoeelarson01 <zoeelarson@gmail.com>
2018-06-04 21:01:27 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -2,5 +2,5 @@ your_favorite_word = "Eleemosynary"
 puts "#{your_favorite_word.upcase}"
 puts "MY FAVORITE WORD IS #{your_favorite_word.upcase}."
 phrase = "I AM NOT SHOUTING"
-# make that downcase
+# make that phrase downcase
 puts lowercased_phrase
