64fb69a879422fb4632ce31b199db09161f2e5e6
zoeelarson01 <zoeelarson@gmail.com>
2018-06-04 20:59:05 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,3 +1,4 @@
 your_favorite_word = "Eleemosynary"
 puts "#{your_favorite_word.upcase}"
 puts "MY FAVORITE WORD IS #{your_favorite_word.upcase}."
+puts "MY FAVORITE WORD IS #{your_favorite_word.upcase}.".downcase
