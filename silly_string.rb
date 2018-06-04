2bbaf8f1b6da944ad3eca68f693dff92ae9d9548
zoeelarson01 <zoeelarson@gmail.com>
2018-06-04 20:32:20 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -4,5 +4,5 @@ puts "IM NOT SHOUTING".downcase
 puts "supercalifragilisticexpialidocious".length
 puts "wow" + "mom"
 puts "i really like programming".capitalize
-puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!".gsub(/)
+puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!".gsub(/[A])
 
