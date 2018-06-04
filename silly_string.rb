fdba0af3fe47ecd5a4011eb0b24f0d16ac503809
zoeelarson01 <zoeelarson@gmail.com>
2018-06-04 20:51:27 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,10 +1,5 @@
 your_favorite_word = "Eleemosynary"
-puts "My favorite word is Eleemosynary
-favorite_word_in_all_caps = #{your_favorite_word}.upcase
-puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}
-puts "IM NOT SHOUTING".downcase
-puts "supercalifragilisticexpialidocious".length
-puts "wow" + "mom"
-puts "i really like programming".capitalize
-puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!".gsub(/[Aa]/, 'A' => 'O', 'a' => 'o')
+"Eleemosynary".upcase
+#{your_favorite_word}.upcase
+
 
